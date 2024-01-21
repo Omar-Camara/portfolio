501961c0662edc1e8271d6917a223169b96f59e0
Omar Camara <117489682+Username273183@users.noreply.github.com>
2024-01-21 17:32:44 -0500

    changes

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add("show");
-//         } else {
-//             entry.target.classList.remove("show");
-//         }
-//     });
-// });
-
-// const hiddenElements = document.querySelectorAll(".hidden");
-// hiddenElements.forEach((el) => {
-//     observer.observe(el);
-// });
-// var icon = document.getElementById('icon');
-
-// icon.onclick = function () {
-//     document.body.classList.toggle("light-theme");
-    // if(document.body.classList.contains("light-theme")){
-    //     icon.className = "fa-solid fa-moon fa-2xl";
-    //     icon.style.color = "#000000";
-    //     document.getElementById('ghicon').src = "/images/icons8-github-dark.svg";
-    // }else{
-//         icon.className = "fa-solid fa-sun fa-2xl";
-//         icon.style.color = "#ffffff";
-//         document.getElementById('ghicon').src = "/images/icons8-github-light.svg";
-//     }
-// }
-
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
@@ -46,15 +17,3 @@ themeSwitch.addEventListener('change', () => {
     
     }
 });
-
-// const themeSwitch = document.getElementById('themeSwitch');
-// const parallaxElement = document.getElementById('parallax');
-
-// themeSwitch.addEventListener('change', () => {
-//     document.body.classList.toggle('light-theme');
-//     if (parallaxElement) {
-//         parallaxElement.style.backgroundImage = 'url("images/moon.jpg")';
-//     } else {
-//         console.error('Element with ID "parallax" not found.');
-//     }
-// });
